c5d330e3f4e12ae7ed5cc006400ec521b0f8b7c6
Niv Sardi <xaiki@evilgiggle.com>
2016-01-10 17:30:19 -0300

    init
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ index.js
@@ -0,0 +1,35 @@
+var memoize = require('memoizee');
+
+var Provider = function () {
+    var memopts = {
+        maxAge: 10 * 60 * 1000,
+        /* 10 minutes */
+        preFetch: 0.5,
+        /* recache every 5 minutes */
+        primitive: true
+    };
+
+    this.memfetch = memoize(this.fetch.bind(this), memopts);
+    this.fetch = this._fetch.bind(this);
+
+    this.detail = memoize(this.detail.bind(this), _.extend(memopts, {
+        async: true
+    }));
+};
+
+Provider.prototype._fetch = function (filters) {
+    filters.toString = this.toString;
+    var promise = this.memfetch(filters),
+    _this = this;
+    promise.catch(function (error) {
+        // Delete the cached result if we get an error so retry will work
+        _this.memfetch.delete(filters);
+    });
+    return promise;
+};
+
+Provider.prototype.toString = function (arg) {
+    return JSON.stringify(this);
+};
+
+module.exports = Provider
