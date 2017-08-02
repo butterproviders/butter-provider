eaa6a59026704c90bfad9ff8185ec2830f644c35
Chris Alderson <chrisalderson@protonmail.com>
2017-08-02 21:48:31 +0000

    es6-ify

--- index.js
+++ index.js
@@ -1,206 +1,226 @@
-var assign = Object.assign || require('es6-object-assign').assign
-var memoize = require('memoizee')
+'use strict'
 
-var parseArgForType = function (type, arg) {
-    try {
-        switch (type) {
-        case Provider.ArgType.NUMBER:
-            return Number(arg)
-            break
-        case Provider.ArgType.ARRAY:
-            return JSON.parse(arg)
-            break
-        case Provider.ArgType.OBJECT:
-            return JSON.parse(arg)
-            break
-        case Provider.ArgType.BOOLEAN:
-            return !!arg
-            break
-        case Provider.ArgType.STRING:
-        default:
-            return arg
-            break
-        }
-    } catch(e) {
-        console.error("Error Parsing args", args, k, e)
-    }
+const memoize = require('memoizee')
+
+const defaultArgs = {
+  memops: {
+    maxAge: 10 * 60 * 1000,
+    /* 10 minutes */
+    preFetch: 0.5,
+    /* recache every 5 minutes */
+    primitive: true,
+    promise: true
+  }
 }
 
-var parseArgs = function (config, uri) {
-    var tokenize = uri.split('?')
+const defaultConfig = {
+  args: {},
+  filters: {}
+}
+
+class Provider {
+
+  constructor(args = defaultArgs, config = defaultConfig) {
+    this.args = args
+    this.config = config
+
+    const memopts = args.memops
+
+    this.args = Object.assign(
+      this.args,
+      this._processArgs(this.config, this.args)
+    )
+    this.filters = Object.assign(
+      {},
+      Provider.DefaultFilters,
+      this.config.filters
+    )
+
+    this.fetch = this._makeCached(this.fetch, memopts)
+    this.detail = this._makeCached(this.detail, memopts)
+  }
+
+  _makeCached(method, memopts) {
+    const memoizedMethod = memoize(method, memopts)
+
+    return function() {
+      // XXX: Should be replaced with spread operator if possible.
+      return memoizedMethod.apply(this, arguments)
+        .catch(error => {
+          // Delete the cached result if we get an error so retry will work
+          method.delete(this.filters)
+          return Promise.reject(error)
+        })
+    }
+  }
+
+  _parseArgForType(type, args) {
+    try {
+      switch (type) {
+        case Provider.ArgType.NUMBER:
+          return Number(args)
+        case Provider.ArgType.ARRAY:
+          return JSON.parse(args)
+        case Provider.ArgType.OBJECT:
+          return JSON.parse(args)
+        case Provider.ArgType.BOOLEAN:
+          return !!args
+        case Provider.ArgType.STRING:
+        default:
+          return args
+      }
+    } catch (e) {
+      console.error('Error Parsing args', args, e)
+    }
+  }
+
+  _parseArgs(config, uri) {
+    const tokenize = uri.split('?')
 
     config = config || {args: {}}
     config.args = config.args || {}
 
-    // XXX:reimplement querystring.parse to not escape
-    var args = {}
-    tokenize[1] && tokenize[1].split('&').map(function (v){
-        var m = v.split('=')
-        args[m[0]]= parseArgForType(config.args[m[0]], m[1])
+    // XXX: Reimplement querystring.parse to not escape
+    const args = {}
+
+    // XXX: This really isn't readable code.
+    tokenize[1] && tokenize[1].split('&').map(v => {
+      const m = v.split('=')
+      args[m[0]] = this._parseArgForType(config.args[m[0]], m[1])
     })
 
     return args
-}
+  }
 
-var processArgs = function (config, args) {
-    if (typeof(args) === 'string') { // we got a URI
-        args = parseArgs(config, args)
+  _processArgs(config, args) {
+    if (typeof args === 'string') {
+      // We got a URI
+      args = this._parseArgs(config, args)
     }
 
-    Object.keys(config.args).map(function(k) {
-        if (! args || ! args[k]) {
-            console.error ('value', k, 'was not provided')
-            return
-        }
+    Object.keys(config.args).map(k => {
+      if (!args || !args[k]) {
+        console.error(`value ${k} was not provided`)
+      }
     })
 
-    return assign({}, config.defaults, args)
-}
+    return Object.assign({}, config.defaults, args)
+  }
 
-var _make_cached = function (method, memopts) {
-    var _this = this,
-        memoized_method = memoize(method.bind(this), memopts)
+  _warnDefault(fn, support) {
+    console.warn('you are using the default ' + fn + ' implementation,')
 
-    return function () {
-        return memoized_method.apply(this, arguments)
-            .catch(function (error) {
-                // Delete the cached result if we get an error so retry will work
-                method.delete(filters)
-                return Promise.reject(error)
-            })
+    if (support) {
+      console.warn(
+        `you will probably want to use your own to support: ${support}.`
+      )
     }
-}
+  }
 
-var Provider = function (args, defaultConfig) {
-    args = args || {};
-    var config  = this.config || defaultConfig || {}
-    config.args = config.args || {}
-    this.config = config
+  _randomArray(a) {
+    return a[Math.floor(Math.random() * a.length)]
+  }
 
-    var memopts = args.memops || {
-        maxAge: 10 * 60 * 1000,
-        /* 10 minutes */
-        preFetch: 0.5,
-        /* recache every 5 minutes */
-        primitive: true,
-        promise: true
-    }
+  resolveStream (src, config, data) {
+    this._warnDefault('resolveStream', 'multiple languages')
 
-    this.args = assign({}, this.args, processArgs(config, args))
-    this.filters = assign({}, Provider.DefaultFilters, config.filters)
+    return src
+  }
 
-    var cacher = _make_cached.bind(this)
-    this.fetch  = cacher(this.fetch,  memopts)
-    this.detail = cacher(this.detail, memopts)
+  random() {
+    this._warnDefault('random', 'faster random')
+
+    const uniqueId = this.config.uniqueId
+    return this.fetch()
+      .then(data => this._randomArray(data.results))
+      .then(data => this.detail(data[uniqueId], data))
+  }
+
+  extractIds(items) {
+    this._warnDefault('extractIds')
+
+    return items.results.map(r => r[this.config.uniqueId])
+  }
+
+  detail(id, oldData) {
+    this._warnDefault('detail', 'better performing fetch and detail calls')
+    return Promise.resolve(oldData)
+  }
+
+  toString(arg) {
+    return JSON.stringify(this)
+  }
+
 }
 
 Provider.DefaultFilters = {
-    genres: {
-        all:         'All',
-        action:      'Action',
-        adventure:   'Adventure',
-        animation:   'Animation',
-        biography:   'Biography',
-        comedy:      'Comedy',
-        crime:       'Crime',
-        documentary: 'Documentary',
-        drama:       'Drama',
-        family:      'Family',
-        fantasy:     'Fantasy',
-        filmNoir:    'Film-Noir',
-        history:     'History',
-        horror:      'Horror',
-        music:       'Music',
-        musical:     'Musical',
-        mystery:     'Mystery',
-        romance:     'Romance',
-        sciFi:       'Sci-Fi',
-        short:       'Short',
-        sport:       'Sport',
-        thriller:    'Thriller',
-        war:         'War',
-        western:     'Western'
-    },
-    sorters: {
-        popularity:  'Popularity',
-        trending:    'Trending',
-        lastAdded:   'Last Added',
-        year:        'Year',
-        title:       'Title',
-        rating:      'Rating'
-    }
+  genres: {
+    all: 'All',
+    action: 'Action',
+    adventure: 'Adventure',
+    animation: 'Animation',
+    biography: 'Biography',
+    comedy: 'Comedy',
+    crime: 'Crime',
+    documentary: 'Documentary',
+    drama: 'Drama',
+    family: 'Family',
+    fantasy: 'Fantasy',
+    filmNoir: 'Film-Noir',
+    history: 'History',
+    horror: 'Horror',
+    music: 'Music',
+    musical: 'Musical',
+    mystery: 'Mystery',
+    romance: 'Romance',
+    sciFi: 'Sci-Fi',
+    short: 'Short',
+    sport: 'Sport',
+    thriller: 'Thriller',
+    war: 'War',
+    western: 'Western'
+  },
+  sorters: {
+    popularity: 'Popularity',
+    trending: 'Trending',
+    lastAdded: 'Last Added',
+    year: 'Year',
+    title: 'Title',
+    rating: 'Rating'
+  }
 }
 
 Provider.ArgType = {
-    ARRAY:   'BUTTER_PROVIDER_ARG_TYPE_ARRAY',
-    OBJECT:  'BUTTER_PROVIDER_ARG_TYPE_OBJECT',
-    STRING:  'BUTTER_PROVIDER_ARG_TYPE_STRING',
-    BOOLEAN: 'BUTTER_PROVIDER_ARG_TYPE_BOOLEAN',
-    NUMBER:  'BUTTER_PROVIDER_ARG_TYPE_NUMBER'
+  ARRAY: 'BUTTER_PROVIDER_ARG_TYPE_ARRAY',
+  OBJECT: 'BUTTER_PROVIDER_ARG_TYPE_OBJECT',
+  STRING: 'BUTTER_PROVIDER_ARG_TYPE_STRING',
+  BOOLEAN: 'BUTTER_PROVIDER_ARG_TYPE_BOOLEAN',
+  NUMBER: 'BUTTER_PROVIDER_ARG_TYPE_NUMBER'
 }
 
 Provider.ItemType = {
-    MOVIE: 'movie',
-    TVSHOW: 'tvshow'
+  MOVIE: 'movie',
+  TVSHOW: 'tvshow'
 }
 
 Provider.OrderType = {
-    ASC: 'asc',
-    DESC: 'desc',
-    NULL: null
+  ASC: 'asc',
+  DESC: 'desc',
+  NULL: null
 }
 
 Provider.SorterType = {
-    NAME: 'name',
-    RATING: 'rating',
-    POPULARITY: 'popularity',
-    NULL: null
+  NAME: 'name',
+  RATING: 'rating',
+  POPULARITY: 'popularity',
+  NULL: null
 }
 
 Provider.QualityType = {
-    DEFAULT: '0',
-    LOW: '480p',
-    MEDIUM: '720p',
-    HIGH: '1080p'
-}
-
-function warnDefault(fn, support) {
-    console.warn ('you are using the default ' + fn + ' implementation,')
-    if (support)
-        console.warn ('you will probably want to use your own to support:' + support + '.')
-}
-
-function randomArray(a) {
-    return a[Math.ceil(Math.random(a.length))]
-}
-
-Provider.prototype.resolveStream = function (src, config, data) {
-    warnDefault('resolveStream', 'multiple languages')
-    return src
-}
-
-Provider.prototype.random = function () {
-    warnDefault('random', 'faster random')
-    var uniqueId = this.config.uniqueId
-    var detail = this.detail.bind(this)
-    return this.fetch.apply(this)
-        .then(function (data) {return randomArray(data.results)})
-        .then(function (data) {return detail(data[uniqueId], data)})
-}
-
-Provider.prototype.extractIds = function (items) {
-    warnDefault('extractIds')
-    var id = this.config.uniqueId
-    return items.results.map(function (r) { return r[id]})
-}
-
-Provider.prototype.detail = function (id, old_data) {
-    warnDefault('detail', 'better performing fetch and detail calls')
-    return Promise.resolve(old_data)
-}
-
-Provider.prototype.toString = function (arg) {
-    return JSON.stringify(this)
+  DEFAULT: '0',
+  LOW: '480p',
+  MEDIUM: '720p',
+  HIGH: '1080p'
 }
 
 module.exports = Provider
--- tests/basic.js
+++ tests/basic.js
@@ -1,182 +1,183 @@
-var path = require('path')
-var tape = require('tape')
-var Provider = require('butter-provider')
-var debug = require('debug')('butter-provider:tests')
+'use strict'
 
-var pkg = require(path.join(process.cwd(), 'package.json'))
+const debug = require('debug')('butter-provider:tests')
+const path = require('path')
+const Provider = require('butter-provider')
+const tape = require('tape')
 
-var config = {
-    args: {},
-    timeout: 1000
+const pkg = require(path.join(process.cwd(), 'package.json'))
+
+let config = {
+  args: {},
+  timeout: 1000
 }
 
 if (pkg.butter) {
-    config = Object.assign({}, config, pkg.butter)
+  config = Object.assign({}, config, pkg.butter)
 }
 
-function load() {
-    return require(process.cwd())
+function load () {
+  return require(process.cwd())
 }
 
-function isEmpty(obj) {
-    return Object.keys(obj).length === 0 && obj.constructor === Object
+function isEmpty (obj) {
+  return Object.keys(obj).length === 0 && obj.constructor === Object
 }
 
-function instanciate(loadFunction) {
-    if (isEmpty(loadFunction)) {
-        loadFunction = load;
+function instanciate (loadFunction) {
+  if (isEmpty(loadFunction)) {
+    loadFunction = load
+  }
+
+  const P = loadFunction()
+  const testArgs = pkg.butter ? pkg.butter.testArgs : null
+
+  return new P(testArgs)
+}
+
+function isInValues (element, set) {
+  return Object.keys(set).reduce((a, c) => a || (element === set[c]))
+}
+
+function getRandomKey (array) {
+  return ~~(Math.random() * (array.length))
+}
+
+function getRandom (array) {
+  return array[getRandomKey(array)]
+}
+
+function runAllTests (loadFunction) {
+  if (isEmpty(loadFunction)) {
+    loadFunction = load
+  }
+
+  function testDetail (t, d, uniqueId) {
+    // console.log('checking details for', d, uniqueId)
+    t.ok(d, 'we were able to get details')
+    t.ok(d[uniqueId] || d.id, 'we have an unique id')
+    t.ok(d.title, 'we have a title')
+    t.ok(d.year, 'we have a year')
+    t.ok(d.genres, 'we have a genres field')
+    t.ok(d.genres.length > 0, 'we have at least 1 genre')
+    t.ok(d.rating, 'we have a rating')
+    t.ok(d.backdrop, 'we have a backdrop')
+    t.ok(d.poster, 'we have a poster')
+    t.ok(d.subtitle, 'we have a subtitle')
+    t.ok(d.synopsis, 'we have a synopsis')
+    t.ok(typeof (d.synopsis) === 'string', 'synopsis is a string')
+    t.ok(d.synopsis.length > 16, 'synopsis is at least 16 bytes')
+    t.ok(d.runtime, 'we have a runtime')
+
+    const type = d.type
+    t.ok(isInValues(type, Provider.ItemType),
+      'we have a type field which is an item type')
+
+    if (type === Provider.ItemType.MOVIE) {
+      t.ok(d.trailer || d.trailer === false, 'we have a trailer')
+
+      t.ok(d.torrents, 'we have a torrents field')
+      const quality = getRandom(Object.keys(d.torrents))
+      t.ok(isInValues(quality, Provider.QualityType),
+           'we have a quality which is a quality type')
+      t.ok(d.torrents[quality], 'we have a quality object')
+      t.ok(d.torrents[quality].url, 'we have an url to stream')
+      t.ok(d.torrents[quality].size, 'we have the size')
+    } else if (type === Provider.ItemType.TVSHOW) {
+      t.ok(d.status, 'we have a status')
+      t.ok(d.num_seasons, 'we have a num_seasons field')
+      t.ok(d.episodes, 'we have an episodes field')
+      t.ok(d.episodes.length > 0, 'we have at least 1 episode')
+
+      const episode = getRandom(d.episodes)
+      if (!episode) {
+        console.log(`now d is ${d}`)
+      }
+      t.ok(episode.first_aired, 'we have a first aired field')
+      t.ok(episode.overview, 'we have an overview')
+      t.ok(isFinite(episode.episode), 'we have an episode number')
+      t.ok(episode.season, 'we have a season number')
+      t.ok(episode.tvdb_id, 'we have a tvdb id')
+
+      t.ok(episode.torrents, 'we have a torrents field')
+
+      const quality = getRandom(Object.keys(d.torrents))
+      t.ok(isInValues(quality, Provider.QualityType),
+        'we have a quality which is a quality type')
+      t.ok(episode.torrents[quality], 'we have a quality object')
+      t.ok(episode.torrents[quality].url, 'we have an url to stream')
+    } else {
+      t.notOk(type, 'is not a valid type')
     }
+  }
 
-    var P = loadFunction()
-    var testArgs = pkg.butter?pkg.butter.testArgs:null
+  tape.onFinish(() => process.exit(0))
 
-    return new P(testArgs)
-}
+  tape('loads', t => {
+    console.log(loadFunction)
+    const P = loadFunction()
 
-function isInValues(element, set) {
-    return Object.keys(set).reduce(function (a, c) {
-        return a || (element === set[c])
-    })
-}
+    t.ok(P, 'we were able to load')
 
-function getRandomKey(array) {
-    return ~~(Math.random()*(array.length))
-}
+    const I = instanciate(loadFunction)
 
-function getRandom(array) {
-    return array[getRandomKey(array)];
-}
+    t.ok(I, 'we were able to instanciate')
 
-var runAllTests = function(loadFunction) {
-    if (isEmpty(loadFunction)) {
-        loadFunction = load;
-    }
+    t.ok(I.config.name, 'we have a name')
+    t.ok(I.config.uniqueId, 'we have a uniqueId')
+    t.ok(I.config.tabName, 'we have a tabName')
 
-    function testDetail(t, d, uniqueId) {
-        //    console.log('checking details for', d, uniqueId)
-        t.ok(d, 'we were able to get details')
-        t.ok(d[uniqueId] || d.id, 'we have an unique id')
-        t.ok(d.title, 'we have a title')
-        t.ok(d.year, 'we have a year')
-        t.ok(d.genres, 'we have a genres field')
-        t.ok(d.genres.length > 0, 'we have at least 1 genre')
-        t.ok(d.rating, 'we have a rating')
-        t.ok(d.backdrop, 'we have a backdrop')
-        t.ok(d.poster, 'we have a poster')
-        t.ok(d.subtitle, 'we have a subtitle')
-        t.ok(d.synopsis, 'we have a synopsis')
-        t.ok(typeof (d.synopsis) === 'string', 'synopsis is a string')
-        t.ok(d.synopsis.length > 16, 'synopsis is at least 16 bytes')
-        t.ok(d.runtime, 'we have a runtime')
+    t.ok(I.args, 'we have an args object')
 
-        var type = d.type
-        t.ok(isInValues(type, Provider.ItemType), 'we have a type field which is an item type')
+    t.end()
+  })
 
-        if (type === Provider.ItemType.MOVIE) {
-            t.ok(d.trailer || d.trailer === false, 'we have a trailer')
+  tape('fetch', t => {
+    debug('fetch, timeout', config.timeout)
+    t.timeoutAfter(config.timeout)
 
-            t.ok(d.torrents, 'we have a torrents field')
-            var quality = getRandom(Object.keys(d.torrents))
-            t.ok(isInValues(quality, Provider.QualityType),
-                 'we have a quality which is a quality type')
-            t.ok(d.torrents[quality], 'we have a quality object')
-            t.ok(d.torrents[quality].url, 'we have an url to stream')
-            t.ok(d.torrents[quality].size, 'we have the size')
-        } else if (type===Provider.ItemType.TVSHOW) {
-            t.ok(d.status, 'we have a status')
-            t.ok(d.num_seasons, 'we have a num_seasons field')
-            t.ok(d.episodes, 'we have an episodes field')
-            t.ok(d.episodes.length > 0, 'we have at least 1 episode')
+    const I = instanciate(loadFunction)
 
-            var episode = getRandom(d.episodes);
-            if (!episode) { console.log ('now d is ', d)}
-            t.ok(episode.first_aired, 'we have a first aired field')
-            t.ok(episode.overview, 'we have an overview')
-            t.ok(isFinite(episode.episode), 'we have an episode number')
-            t.ok(episode.season, 'we have a season number')
-            t.ok(episode.tvdb_id, 'we have a tvdb id')
+    I.fetch().then(r => {
+      debug('fetch', r)
+      t.ok(r, 'we were able to fetch')
+      t.ok(r.hasMore === true || r.hasMore === false,
+        'we have a hasMore field that is a boolean')
+      t.ok(r.results, 'we have a results field')
+      t.ok(r.results.length > 0, 'we have at least 1 result')
 
-            t.ok(episode.torrents, 'we have a torrents field')
-            var quality = getRandom(Object.keys(d.torrents))
-            t.ok(isInValues(quality, Provider.QualityType),
-                 'we have a quality which is a quality type')
-            t.ok(episode.torrents[quality], 'we have a quality object')
-            t.ok(episode.torrents[quality].url, 'we have an url to stream')
-        } else {
-            t.notOk(type, 'is not a valid type')
-        }
-    }
-
-
-    tape.onFinish(function() {
-        process.exit(0)
-    })
-
-    tape('loads', function (t) {
-        console.log(loadFunction)
-        var P = loadFunction()
-
-        t.ok(P, 'we were able to load')
-
-        var I = instanciate(loadFunction)
-
-        t.ok(I, 'we were able to instanciate')
-
-        t.ok(I.config.name, 'we have a name')
-        t.ok(I.config.uniqueId, 'we have a uniqueId')
-        t.ok(I.config.tabName, 'we have a tabName')
-
-        t.ok(I.args, 'we have an args object')
-
+      const uniqueIds = I.extractIds(r)
+      const key = getRandomKey(uniqueIds)
+      console.log('will try to get details for key', key)
+      t.ok(uniqueIds, 'extractIds')
+      I.detail(uniqueIds[key], r.results[key]).then(d => {
+        debug('detail', d)
+        testDetail(t, d, I.config.uniqueId)
         t.end()
+      }).catch(e => {
+        console.log('ERROR in details', e)
+        t.notOk(e, 'failed detail')
+      })
+    }).catch(e => {
+      console.log('ERROR in fetch', e)
+      t.notOk(e, 'failed fetch')
     })
+  })
 
-    tape('fetch', function (t) {
-        debug('fetch, timeout', config.timeout)
-        t.timeoutAfter(config.timeout)
+  tape('random', t => {
+    debug('random, timeout', config.timeout)
+    t.timeoutAfter(config.timeout)
 
-        var I = instanciate(loadFunction)
+    var I = instanciate(loadFunction)
 
-        I.fetch().then(function (r) {
-            debug ('fetch', r)
-            t.ok(r, 'we were able to fetch')
-            t.ok(r.hasMore===true || r.hasMore===false, 'we have a hasMore field that is a boolean')
-            t.ok(r.results, 'we have a results field')
-            t.ok(r.results.length > 0, 'we have at least 1 result')
-
-            var uniqueIds = I.extractIds(r)
-            var key = getRandomKey(uniqueIds)
-            console.log('will try to get details for key', key)
-            t.ok(uniqueIds, 'extractIds')
-            I.detail(uniqueIds[key], r.results[key]).then(function (d) {
-                debug ('detail', d)
-                testDetail(t, d, I.config.uniqueId)
-                t.end()
-            }).catch(function (e) {
-                console.log('ERROR in details', e)
-                t.notOk(e, 'failed detail')
-            })
-        }).catch(function (e) {
-            console.log('ERROR in fetch', e)
-            t.notOk(e, 'failed fetch')
-        })
-    })
-
-    tape('random', function (t) {
-        debug('random, timeout', config.timeout)
-        t.timeoutAfter(config.timeout)
-
-        var I = instanciate(loadFunction)
-
-        I.random().then(function (r) {
-            debug ('random', r)
-            testDetail(t, r, I.config.uniqueId)
-            t.end()
-        }).catch(function (e) {
-            t.notOk(e, 'failed random')
-        })
-    })
+    I.random().then(r => {
+      debug('random', r)
+      testDetail(t, r, I.config.uniqueId)
+      t.end()
+    }).catch(e => t.notOk(e, 'failed random'))
+  })
 }
 
-runAllTests.apply(this, arguments);
-module.exports = runAllTests;
+runAllTests.apply(this, arguments)
+
+module.exports = runAllTests
